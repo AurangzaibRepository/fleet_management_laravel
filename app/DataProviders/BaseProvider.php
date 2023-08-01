8d0bd7c7825c10d4bfccca12a5ecdda06a79f88b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 20:27:53 +0500

    Implements base class functionf or get reaystr

--- app/DataProviders/BaseProvider.php
+++ app/DataProviders/BaseProvider.php
@@ -2,6 +2,21 @@
 
 namespace App\DataProviders;
 
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Http;
+
 class BaseProvider
 {
+    protected function getRequest(string $url): JsonResponse
+    {
+        $url = config('app.fleetio_base_url').$url;
+
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Token '.config('app.fleetio_api_key'),
+            'Account-Token' => config('app.fleetio_account_token'),
+        ])->get($url);
+
+        return $response;
+    }
 }
