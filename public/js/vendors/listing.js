b262f29e56cbb2009cda8516dba28f07f76ac313
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 21:58:57 +0500

    Implements custom rendering of last column

--- public/js/vendors/listing.js
+++ public/js/vendors/listing.js
@@ -12,6 +12,14 @@ function populateVendors() {
             {'targets': 0, 'width': '6%', className: 'text-center'},
             {'targets': 2, 'width': '30%'},
             {'targets': [1,3,4], 'width': '20%'},
+            {'targets': 5, className: 'text-center',
+                data:null, 'width': '6%',
+                render: function(row, data) {
+                    return `
+                    <a href="/vendors/${row[5]}"><i class="far fa-question-circle"></i></a>
+                    `;
+                }
+            }
         ],
     });
 }
