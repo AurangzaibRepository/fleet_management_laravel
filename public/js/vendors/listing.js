90f14237bd664a8a1c84577427da55b60a794ac3
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 22:06:45 +0500

    Amends width

--- public/js/vendors/listing.js
+++ public/js/vendors/listing.js
@@ -11,9 +11,9 @@ function populateVendors() {
         columnDefs: [
             {'targets': 0, 'width': '6%', className: 'text-center'},
             {'targets': 2, 'width': '30%'},
-            {'targets': [1,3,4], 'width': '20%'},
+            {'targets': [1,3,4], 'width': '17%'},
             {'targets': 5, className: 'text-center',
-                data:null, 'width': '8%',
+                data:null, 'width': '6%',
                 render: function(row, data) {
                     return `
                     <a href="/vendors/${row[5]}"><i class="fas fa-eye"></i></a>
