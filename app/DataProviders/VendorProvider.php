bc13a9846b1aee20f4c2b1bf3cf526f608b21722
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 22:13:45 +0500

    Implements procider function to get details

--- app/DataProviders/VendorProvider.php
+++ app/DataProviders/VendorProvider.php
@@ -2,14 +2,19 @@
 
 namespace App\DataProviders;
 
-use Illuminate\Http\JsonResponse;
-
 class VendorProvider extends BaseProvider
 {
     public function fetchList(): array
     {
         $response = $this->getRequest(config('app.vendor_list_url'));
-    
+
+        return $response;
+    }
+
+    public function fetchDetails(int $id): array
+    {
+        $response = $this->getRequest(config('app.vendor_detail_url'));
+
         return $response;
     }
 }
