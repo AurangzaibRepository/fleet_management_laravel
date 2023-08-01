3c2f3d22b0ff6adae7910e8b7d271aecdca4fb0f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 22:29:09 +0500

    Includes style

--- resources/views/vendors/details.blade.php
+++ resources/views/vendors/details.blade.php
@@ -1,5 +1,13 @@
 @extends('/layouts/main')
+@push('styles')
+<link rel="stylesheet" href="{{asset('/css/vendors/details.css')}}" />
+@endPush
 
 @section('contents')
 {{Form::label('lbl-page-header', 'Vendor Details', ['id' => 'lbl-page-header'])}}
+<div class="dv-base">
+    <div class="page-layout">
+
+    </div>
+</div>
 @endSection
