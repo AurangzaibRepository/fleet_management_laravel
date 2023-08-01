3056f308b959da6702f1aaad2208914e8f2ebbb7
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 14:52:08 +0500

    Defines rules

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -9,6 +9,8 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
+            'email' => 'required|email|exists:users',
+            'password' => 'required',
         ];
     }
 }
