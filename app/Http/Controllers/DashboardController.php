866772291f9520e9c8da6e60f66706e8b2b3cb7d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 14:22:58 +0500

    Removes divs

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,20 +15,8 @@ class DashboardController extends Controller
     // Default function index
     public function index()
     {
-        $currentUserList = $this->dashboard->getCurrentUsers();
-        $newUserList = $this->dashboard->getNewUsers();
-        $whatsAppSessionList = $this->dashboard->getWhatsAppSessions();
-
         return view('/dashboard', [
-            'pageTitle' => config('app.name').' - Dashboard',
-            'currentUserList' => $currentUserList,
-            'newUserList' => $newUserList,
-            'whatsAppSessionList' => $whatsAppSessionList,
+            'pageTitle' => config('app.name'). ' - Analytics',
         ]);
-    }
-
-    public function activityData(): JsonResponse
-    {
-        return $this->dashboard->getActivityData();
     }
 }
