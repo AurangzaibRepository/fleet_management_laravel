720b07c883c909193c1c5abb29af7f22c42d494f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 20:52:51 +0500

    Implements trasnfomere fo rlist

--- app/Transformers/VendorTransformer.php
+++ app/Transformers/VendorTransformer.php
@@ -4,4 +4,24 @@ namespace App\Transformers;
 
 class VendorTransformer
 {
+    public function transformList(array $data): array
+    {
+        $response = [
+            'data' => [],
+        ];
+
+        foreach ($data as $index => $row) {
+            $address = $row['street_address'].'<br/>'.$row['city'].', '.$row['postal_code'].', '.$row['country'];
+
+            $response['data'][] = [
+                ++$index,
+                $row['name'],
+                $address,
+                $row['phone'],
+                $row['website'],
+            ];
+        }
+
+        return $response;
+    }
 }
