736ad63967b53e2259b88ecc4a6f57cb853f19bd
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 22:01:15 +0500

    Amends font awesome icon

--- public/js/vendors/listing.js
+++ public/js/vendors/listing.js
@@ -13,10 +13,10 @@ function populateVendors() {
             {'targets': 2, 'width': '30%'},
             {'targets': [1,3,4], 'width': '20%'},
             {'targets': 5, className: 'text-center',
-                data:null, 'width': '6%',
+                data:null, 'width': '8%',
                 render: function(row, data) {
                     return `
-                    <a href="/vendors/${row[5]}"><i class="far fa-question-circle"></i></a>
+                    <a href="/vendors/${row[5]}"><i class="fas fa-eye"></i></a>
                     `;
                 }
             }
