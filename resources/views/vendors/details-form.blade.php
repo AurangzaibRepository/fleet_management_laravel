ed6e0eafafa6e2631080ec905ab78a2fd6d6c434
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-01 23:04:23 +0500

    Renders vendor data

--- resources/views/vendors/details-form.blade.php
+++ resources/views/vendors/details-form.blade.php
@@ -1,44 +1,52 @@
 {{Form::open()}}
     <div class="page-layout row">
-        <div class="col-md-2">
+        <div class="col-md-2 d-flex align-items-center">
             {{Form::label('name', 'Name:')}}
         </div>
         <div class="col-md-4">
+            {{Form::text('name', $data->name, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2">
+        <div class="col-md-2 d-flex align-items-center">
             {{Form::label('contact', 'Contact Number:')}}
         </div>
         <div class="col-md-4">
+            {{Form::text('contact', $data->phone, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('contactname', 'Contact Name:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->contact_name, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('contactemail', 'Contact Email:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->contact_email, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('website', 'Website:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->website, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('address', 'Address:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->street_address, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('city', 'City:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->city, ['class' => 'form-control'])}}
         </div>
-        <div class="col-md-2 mt-2">
+        <div class="col-md-2 mt-2 d-flex align-items-center">
             {{Form::label('country', 'Country:')}}
         </div>
         <div class="col-md-4 mt-2">
+            {{Form::text('contact', $data->country, ['class' => 'form-control'])}}
         </div>
     </div>
     {{Form::close()}}
