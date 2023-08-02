4f70a03d0d7d1ddb196054c872cdeec9649fa955
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 12:44:50 +0500

    Includes model

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vehicle;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class VehiclesController extends Controller
 {
+    public function __construct(
+        private Vehicle $vehicle,
+    ) {
+    }
+
     public function index(): view
     {
         return view('vehicles.index', [
