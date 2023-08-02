718edc6c9a1dbf9899bc0a57d6a20b7bf0260656
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 12:06:23 +0500

    Adds table

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -6,4 +6,21 @@
 @section('contents')
 {{Form::label('lbl-page-header', 'Vehicles', ['id' => 'lbl-page-header'])}}
 
+<div class="dv-base">
+    <div class="page-layout">
+        <table id="table-vehicles" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Name</th>
+                    <th>Make</th>
+                    <th>Model</th>
+                    <th>Type</th>
+                    <th></th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</div>
+
 @endSection
