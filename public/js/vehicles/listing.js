d16596b206646f22d858975f6c2fe21cd5187110
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 15:31:00 +0500

    Implements column defs

--- public/js/vehicles/listing.js
+++ public/js/vehicles/listing.js
@@ -8,5 +8,17 @@ function populateVehicles() {
         'bLengthChange': false,
         'bSort': false,
         'ajax': '/vehicles/listing',
+        'columnDefs': [
+            {'targets': 0, 'width': '6%', 'className': 'text-center'},
+            {'targets': [1,2,3,4], 'width': '20%'},
+            {'targets': 5, 'width': '6%',
+                'className': 'text-center', 'data': null,
+                'render': function(row, data) {
+                    return `
+                    <a href="/vehicles/${row[5]}"><i class="fas fa-eye"></i></a>
+                    `;
+                }
+            },
+        ],
     });
 }
