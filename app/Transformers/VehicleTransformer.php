e750acd12d0c3a8e52656b51a1216db48163a7ca
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 20:05:51 +0500

    Implements trasnformer for details

--- app/Transformers/VehicleTransformer.php
+++ app/Transformers/VehicleTransformer.php
@@ -28,4 +28,39 @@ class VehicleTransformer
 
         return $response;
     }
+
+    public function transformDetails(array $data): array
+    {
+        $response = [
+            'id' => $data['id'],
+            'image' => $data['default_image_url_medium'],
+        ];
+
+        $response['details'] = [
+            'name' => $data['name'],
+            'type' => $data['vehicle_type_name'],
+            'year' => $data['year'],
+            'model' => $data['model'],
+            'make' => $data['make'],
+            'vin' => $data['vin'],
+            'meter' => $data['meter_name'],
+        ];
+
+        $response['open_issues'] = [
+            'open' => $data['issues_count'],
+            'orders_count' => $data['work_orders_count'],
+        ];
+
+        $response['dimensions'] = [
+            'height' => $data['height'],
+            'length' => $data['length'],
+            'width' => $data['width'],
+        ];
+
+        $response['engine'] = [
+            'brand' => $data['engine_brand'],
+            'description' => $data['engine_description'],
+            'aspiration' => $data['engine_aspiration'],
+        ];
+    }
 }
