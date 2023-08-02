1065a13a00f548fde7e1a3a8d2a0bb82bde19e94
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 14:54:09 +0500

    Implements function to transform vegicle list

--- app/Transformers/VehicleTransformer.php
+++ app/Transformers/VehicleTransformer.php
@@ -4,4 +4,23 @@ namespace App\Transformers;
 
 class VehicleTransformer
 {
+    public function transformList(array $data): array
+    {
+        $response = [
+            'data' => [],
+        ];
+
+        foreach ($data as $index => $row) {
+            $response['data'][] = [
+                (++$index),
+                $row->name,
+                $row->make,
+                $row->model,
+                $row->type_name,
+                $row->id,
+            ];
+        }
+
+        return $response;
+    }
 }
