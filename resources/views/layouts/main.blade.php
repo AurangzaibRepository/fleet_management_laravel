e375a26df3ef81e1bc62c4ecad6f4646413eb3e0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 18:51:13 +0500

    Includes custom js file

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,6 +36,7 @@
 <script src="{{asset('js/app.js')}}"></script>
 <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
 <script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 <script>
     $.ajaxSetup({
         headers: {
