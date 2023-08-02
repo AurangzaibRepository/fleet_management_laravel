12418d67d7167e57a5980290d285a5541a9e8344
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 12:52:24 +0500

    Includes data provider and transfomer

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\DataProviders\VehicleProvider;
+use App\Transformers\VehicleTransformer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
