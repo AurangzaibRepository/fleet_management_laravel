ed6ce87b51f423fcc5d1d8cecfda80be8c35528f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 13:00:22 +0500

    Implements listing function

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -6,10 +6,25 @@ use App\DataProviders\VehicleProvider;
 use App\Transformers\VehicleTransformer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\JsonResponse;
 
 class Vehicle extends Model
 {
     use HasFactory;
 
     protected $table = null;
+
+    public function __construct(
+        private VehicleProvider $provider,
+        private VehicleTransformer $transformer,
+    ) {
+    }
+
+    public function getListing(): JsonResponse
+    {
+        $data = $this->provider->fetchList();
+        $transformedData = $this->transformer->transformList($data);
+
+        return response()->json($transformedData);
+    }
 }
