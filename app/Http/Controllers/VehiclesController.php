d753e18134b21b9d31b791fd1cba2dd042904461
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 11:55:46 +0500

    Declares index action

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -3,7 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class VehiclesController extends Controller
 {
+    public function index(): view
+    {
+        return view('vehicles.index', [
+            'pageTitle' => config('app.name').' - Vehicles',
+        ]);
+    }
 }
