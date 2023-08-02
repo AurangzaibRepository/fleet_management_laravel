d76c46630d7648bf406f8f6868dc2547803a064d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 12:02:27 +0500

    Includes style file

--- resources/views/vehicles/index.blade.php
+++ resources/views/vehicles/index.blade.php
@@ -1,4 +1,7 @@
 @extends('/layouts/main')
+@push('styles')
+<link rel="stylesheet" href="{{asset('/css/vehicles/listing.css')}}" />
+@endPush
 
 @section('contents')
 {{Form::label('lbl-page-header', 'Vehicles', ['id' => 'lbl-page-header'])}}
