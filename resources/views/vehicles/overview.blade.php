71f8e3306d8a03c47500203362e884f527eb1a77
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 20:24:48 +0500

    Renders fields via loop

--- resources/views/vehicles/overview.blade.php
+++ resources/views/vehicles/overview.blade.php
@@ -3,6 +3,16 @@
         <div class="col-md-6">
             <div class="page-layout">
                 {{Form::label('title', 'Details', ['class' => 'lbl-subtitle'])}}
+                @foreach($data['details'] as $key => $value)
+                <div class="row mt-2">
+                    <div class="col-md-4">
+                        {{Form::label($key, ucfirst($key), ['class' => 'lbl-key'])}}
+                    </div>
+                    <div class="col-md-6">
+                        {{Form::label($key, $value, ['class' => 'lbl-value'])}}
+                    </div>
+                </div>
+                @endForeach
             </div>
         </div>
         <div class="col-md-6">
