b0078529fd3ad85a634da2e4605cb4b498ec2c84
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 16:31:31 +0500

    Assigns id to image

--- resources/views/vehicles/details-form.blade.php
+++ resources/views/vehicles/details-form.blade.php
@@ -1,7 +1,7 @@
 <div class="page-layout">
     <div class="d-flex">
         <div>
-            <img src="{{$data->default_image_url_medium}}" />
+            <img src="{{$data->default_image_url_medium}}" id="img-car" />
         </div>
         <div>
             {{Form::label('name', $data->name, ['id' => 'lbl-name'])}}
