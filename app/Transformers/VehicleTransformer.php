fda3097ba583417f44bee6d3bd6747147c69a1a5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-02 16:05:13 +0500

    Adds image with name

--- app/Transformers/VehicleTransformer.php
+++ app/Transformers/VehicleTransformer.php
@@ -11,9 +11,14 @@ class VehicleTransformer
         ];
 
         foreach ($data as $index => $row) {
+            $name = "<div>
+                <img src='{$row->default_image_url_small}' class='img-car' />
+                <span>{$row->name}</span>
+            </div>";
+
             $response['data'][] = [
                 (++$index),
-                $row->name,
+                $name,
                 $row->make,
                 $row->model,
                 $row->type_name,
