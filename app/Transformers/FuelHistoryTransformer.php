4b0578b607b961af6ed533502430946cd2a7d711
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 14:53:12 +0500

    Implements sloop to get data

--- app/Transformers/FuelHistoryTransformer.php
+++ app/Transformers/FuelHistoryTransformer.php
@@ -9,5 +9,18 @@ class FuelHistoryTransformer
         $response = [
             'data' => [],
         ];
+
+        foreach ($data as $index => $row) {
+            $response['data'][] = [
+                (++$index),
+                $row['vehicle_name'],
+                $row['date'],
+                $row['meter_entry']->value,
+                $row['total_amount'],
+                $row['id'],
+            ];
+        }
+
+        return $response;
     }
 }
