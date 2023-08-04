93ad387c0afdccc2c36ab3532aff3e56926d03ba
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 17:46:23 +0500

    Specifies class

--- resources/views/fuel-history/statistics.blade.php
+++ resources/views/fuel-history/statistics.blade.php
@@ -1,16 +1,16 @@
 <div id="dv-statistics" class="mb-2">
-    <div class="row">
+    <div class="row m-0">
         <div class="col-md-3">
-
+            dfsd
         </div>
         <div class="col-md-3">
-
+            sdfsd
         </div>
         <div class="col-md-3">
-
+            dfds
         </div>
         <div class="col-md-3">
-
+            dfs
         </div>
     </div>
 </div>
