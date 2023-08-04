d64ebed26e10483e3549ac110da9773fe017fb25
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 13:31:09 +0500

    Imports packages

--- app/Models/FuelHistory.php
+++ app/Models/FuelHistory.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\DataProviders\FuelHistoryProvider;
+use App\Transformers\FuelHistoryTransformer;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
