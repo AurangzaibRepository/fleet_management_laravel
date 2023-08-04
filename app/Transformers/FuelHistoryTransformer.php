1f55362d6a97efa943ef990bf74ad228e32a534a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 18:32:52 +0500

    Writes pribate function to calculate statsitics

--- app/Transformers/FuelHistoryTransformer.php
+++ app/Transformers/FuelHistoryTransformer.php
@@ -6,9 +6,8 @@ class FuelHistoryTransformer
 {
     public function transformList(array $data): array
     {
-        $response = [
-            'data' => [],
-        ];
+        $response = $this->calculateStatistics($data);
+        $response['data'] = [];
 
         foreach ($data as $index => $row) {
             $response['data'][] = [
@@ -22,5 +21,20 @@ class FuelHistoryTransformer
         }
 
         return $response;
+    }
+
+    private function calculateStatistics(array $data): array
+    {
+        $dataSize = count($data);
+        $totalFuelCost = array_sum(array_column($data, 'total_amount'));
+        $totalFuelEconomy = array_sum(array_column($data, 'fuel_economy_for_current_user'));
+
+        $response['totalFuelCost'] = config('app.transaction_currency')." {$totalFuelCost}";
+        $response['totalVolume'] = array_sum(array_column($data, 'us_gallons'));
+        $response['avgFuelEconomy'] = $totalFuelEconomy / $dataSize;
+        $response['avgCost'] = $totalFuelCost / $dataSize;
+
+        return $response;
+
     }
 }
