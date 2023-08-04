dffce105833d2fc017c1ec05367d9a389ad491c7
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 11:59:18 +0500

    Includes css

--- resources/views/fuel-history/index.blade.php
+++ resources/views/fuel-history/index.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts/main')
+@push('styles')
+<link rel="stylesheet" href="{{asset('/css/fuel-histories/listing.css')}}" />
+@endPush
 
 @section('contents')
 {{Form::label('lbl-page-header', 'Fuel History', ['id' => 'lbl-page-header'])}}
+
+<div class="dv-base">
+    <div class="page-layout">
+
+    </div>
+</div>
 @endSection
