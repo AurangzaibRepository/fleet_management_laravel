57e2c53c8edff4c05c5d4b67144ac863c26ea06e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 17:07:37 +0500

    Adds column divs

--- resources/views/fuel-history/statistics.blade.php
+++ resources/views/fuel-history/statistics.blade.php
@@ -1,3 +1,16 @@
-<div id="dv-statistics">
+<div id="dv-statistics" class="mb-2">
+    <div class="row">
+        <div class="col-md-3">
 
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+        <div class="col-md-3">
+
+        </div>
+    </div>
 </div>
