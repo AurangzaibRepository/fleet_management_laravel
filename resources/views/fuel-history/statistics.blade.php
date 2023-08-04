8462c9d0ee7431327e7d95f08a94bc02cea0e6b4
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 18:48:37 +0500

    Assign id to lements

--- resources/views/fuel-history/statistics.blade.php
+++ resources/views/fuel-history/statistics.blade.php
@@ -2,19 +2,19 @@
     <div class="row m-0">
         <div class="col-md-3">
             <span>Total Fuel Cost</span>
-            <label></label>
+            <label id="label-total-fuel"></label>
         </div>
         <div class="col-md-3">
             <span>Total Volume</span>
-            <label></label>
+            <label id="label-total-volume"></label>
         </div>
         <div class="col-md-3">
             <span>Avg. Fuel Economy</span>
-            <label></label>
+            <label id="label-avg-fuel"></label>
         </div>
         <div class="col-md-3">
             <span>Avg. Cost</span>
-            <label></label>
+            <label id="label-avg-cost"></label>
         </div>
     </div>
 </div>
