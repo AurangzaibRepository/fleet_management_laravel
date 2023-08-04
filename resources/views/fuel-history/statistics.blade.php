bb49fee1e38b7b54f0dfa3bcb807770088252d55
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 17:59:18 +0500

    Adds label udne rstatistics

--- resources/views/fuel-history/statistics.blade.php
+++ resources/views/fuel-history/statistics.blade.php
@@ -2,15 +2,19 @@
     <div class="row m-0">
         <div class="col-md-3">
             <span>Total Fuel Cost</span>
+            <label></label>
         </div>
         <div class="col-md-3">
             <span>Total Volume</span>
+            <label></label>
         </div>
         <div class="col-md-3">
             <span>Avg. Fuel Economy</span>
+            <label></label>
         </div>
         <div class="col-md-3">
             <span>Avg. Cost</span>
+            <label></label>
         </div>
     </div>
 </div>
