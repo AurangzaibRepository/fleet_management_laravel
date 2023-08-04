65a8c9410238179e10c5e81eba23b1e30c109342
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 11:07:53 +0500

    Includes model

--- app/Http/Controllers/FuelHistoriesController.php
+++ app/Http/Controllers/FuelHistoriesController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\FuelHistory;
 use Illuminate\Http\Request;
 
 class FuelHistoriesController extends Controller
 {
+    public function __construct(
+        private FuelHistory $fuelHistory,
+    ) {
+    }
 }
