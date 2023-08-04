44042929d340755fce665b8b2386f508df64e051
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 13:00:26 +0500

    Includes ajax and width for 2nd column

--- public/js/fuel-histories/listing.js
+++ public/js/fuel-histories/listing.js
@@ -7,9 +7,11 @@ function populateFuelHistory() {
         'bLengthChange': false,
         'bSort': false,
         'searching': false,
+        'ajax': '/fuel-history/listing',
         'columnDefs': [
             {'targets': 0, 'width': '6%', 'className': 'text-center'},
-            {'targets': [1,2,3,4], 'width': '24%'},
+            {'targets': 1, 'width': '30%'},
+            {'targets': [2,3,4], 'width': '20%'},
             {'targets': 5, 'width': '5%',
                 'className': 'text-center', 'data': null,
                 'render': function(row, data) {
