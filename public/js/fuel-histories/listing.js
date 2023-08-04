05ce503a4358b269a6bc92fb4c43d799db2f59aa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-04 12:56:27 +0500

    Adds column definitions ind atatable

--- public/js/fuel-histories/listing.js
+++ public/js/fuel-histories/listing.js
@@ -7,5 +7,17 @@ function populateFuelHistory() {
         'bLengthChange': false,
         'bSort': false,
         'searching': false,
+        'columnDefs': [
+            {'targets': 0, 'width': '6%', 'className': 'text-center'},
+            {'targets': [1,2,3,4], 'width': '24%'},
+            {'targets': 5, 'width': '5%',
+                'className': 'text-center', 'data': null,
+                'render': function(row, data) {
+                    return `
+                        <a href="/fuel-history/${row[5]}"><i class="fas fa-eye"></i></a>
+                    `;
+                },
+            },
+        ],
     });
 }
