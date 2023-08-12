5cccb52ba6ef11e69ab48d8f6c8225330c95cc13
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-12 09:42:34 +0500

    Adds divs

--- resources/views/fuel-history/details-form.blade.php
+++ resources/views/fuel-history/details-form.blade.php
@@ -1,3 +1,14 @@
 <div class="page-layout">
     {{Form::label('name', 'Fuel Entry #'.$data['id'], ['id' => 'module-header'])}}
 </div>
+
+<div class="mt-4">
+    <div class="row">
+        <div class="col-md-6">
+
+        </div>
+        <div class="col-md-6">
+
+        </div>
+    </div>
+</div>
