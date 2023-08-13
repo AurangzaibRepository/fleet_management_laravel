c34fb62003635dd39611a88827fafbe41b1b61f9
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-08-13 10:08:46 +0500

    Renders specifiactions data

--- resources/views/fuel-history/specs.blade.php
+++ resources/views/fuel-history/specs.blade.php
@@ -1,3 +1,11 @@
 <div class="page-layout">
-    {{Form::label('title', 'Specification', ['class' => 'lbl-subtitle'])}}
+    <div class="row">
+    @foreach($data['specifications'] as $key => $value)
+    <div class="col-md-4">
+        {{Form::label($key, formatKeyLabel($key), ['class' => 'lbl-key'])}}
+        <br/>
+        {{Form::label($key, $value, ['class' => 'lbl-value bold'])}}
+    </div>
+    @endforeach
+    </div>
 </div>
